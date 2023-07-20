5468f53ac6d4d2a45395015ce3066bdd3df3fa1b
Juan Rodriguez <75664608+sowkee@users.noreply.github.com>
2023-07-19 21:47:01 -0500

    delete
    delete user package

--- src/main/java/com/credibanco/loginback/user/application/exception/EmptyEntityException.java
+++
@@ -1,7 +0,0 @@
-package com.credibanco.loginback.user.application.exception;
-
-public class EmptyEntityException extends Exception{
-    public EmptyEntityException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/credibanco/loginback/user/application/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.credibanco.loginback.user.application.exception;
-
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
